09d778c16eaffffc5cc10dad91352f76679be10e
Shanti Chellaram <shanti@shanti.im>
2020-02-08 12:24:38 -0500

    Bump to 0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Fds.MixProject do
   def project do
     [
       app: :fds,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       description: "Erlang/Elixir's missing fast / functional data structures",
